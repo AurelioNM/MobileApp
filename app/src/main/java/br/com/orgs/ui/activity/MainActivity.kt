5eb02a73817d21f80fd0fd2471fd1860329544e0
AurelioNM <aureliomororo300@gmail.com>
2022-03-05 17:36:48 -0300

    Adding fields and button to layoutForm

--- app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
@@ -1,25 +1,17 @@
 package br.com.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.orgs.R
 import br.com.orgs.model.Produto
 import br.com.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de Frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e maçã"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99 "
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
             Produto(
