a119d22e52de0f3dbf94b0127e3e9fad66c6c537
AurelioNM <aureliomororo300@gmail.com>
2022-03-05 21:06:51 -0300

    Option to add more items to the list ready and Refactoring Activities and Adapter

---
+++ app/src/main/java/br/com/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,19 @@
+package br.com.orgs.dao
+
+import br.com.orgs.model.Produto
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto) {
+        produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+
+}
