84d134f14ab36fddf252698cfc7c5ce56b63d214
AurelioNM <aureliomororo300@gmail.com>
2022-03-12 16:23:06 -0300

    Adding sample project with deaful image

--- app/src/main/java/br/com/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.orgs.dao
 
 import br.com.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,7 +14,13 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçã e uva",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
 
 }
