79ed58414bda269caef1cfe4ec73b3ef78b48808
AurelioNM <aureliomororo300@gmail.com>
2022-03-12 18:04:32 -0300

    Formatting price field of card

--- app/src/main/java/br/com/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -2,13 +2,13 @@ package br.com.orgs.ui.recyclerview.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import br.com.orgs.R
 import br.com.orgs.databinding.ProdutoItemBinding
 import br.com.orgs.model.Produto
+import java.math.BigDecimal
+import java.text.NumberFormat
+import java.util.*
 
 class ListaProdutosAdapter(
     private val context: Context,
@@ -26,7 +26,14 @@ class ListaProdutosAdapter(
             val descricao = binding.produtoItemDescricao
             descricao.text = produto.descricao
             val valor = binding.produtoItemValor
-            valor.text = produto.valor.toPlainString()
+            val valorEmMoeda = formataParaModedaBrasileira(produto.valor)
+            valor.text = valorEmMoeda
+        }
+
+        private fun formataParaModedaBrasileira(valor: BigDecimal): String? {
+            val formatador = NumberFormat
+                .getCurrencyInstance(Locale("pt", "br"))
+            return formatador.format(valor)
         }
 
     }
